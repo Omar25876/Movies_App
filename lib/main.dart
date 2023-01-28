328a935f194b312982c1bdba2836a9605e4a2557
Omar Mohamed Ibrahim <cooool159357@gmail.com>
2023-01-28 16:40:21 +0200

    initial movies app

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,6 +18,7 @@ class MyApp extends StatelessWidget {
         HomeMainScreen.routeName:(context) => HomeMainScreen(),
 
       },
+      title: 'Movies',
       initialRoute: HomeMainScreen.routeName,
       theme: MyTheme.Light,
       themeMode: ThemeMode.light,
