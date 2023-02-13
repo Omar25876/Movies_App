e8521583455d35a4dbf0746e8f7afa87d3110438
Omar Mohamed Ibrahim <cooool159357@gmail.com>
2023-02-14 01:55:34 +0200

    Finish Movies App

--- lib/ui/screens/watchlist_fragment/watchlist_fragment.dart
+++ lib/ui/screens/watchlist_fragment/watchlist_fragment.dart
@@ -14,22 +14,47 @@ class WatchlistTab extends StatefulWidget {
 class _WatchlistTabState extends State<WatchlistTab> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Movie>>(
-      stream: DatabaseUtils.readMoviesFormFirebase(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        }
-        List<Movie> messages =
-            snapshot.data?.docs.map((task) => task.data()).toList() ?? [];
-        return  ListView.separated(
-          separatorBuilder: (context, index) => Divider(color: Colors.grey,thickness: 1,),
-          itemCount: messages.length,
-          itemBuilder: (d, index) {
-            return SearchWidget(messages[index]);
-          },
-        );
-      },
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(18, 19, 18, 1.0),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('WatchList',style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold
+              ),),
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot<Movie>>(
+                stream: DatabaseUtils.readMoviesFormFirebase(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator(
+                      color: Color.fromRGBO(255, 187, 59, 1.0),
+                    ));
+                  }
+                  List<Movie> messages =
+                      snapshot.data?.docs.map((task) => task.data()).toList() ?? [];
+                  return  ListView.separated(
+                    separatorBuilder: (context, index) => Divider(color: Colors.grey,thickness: 1,),
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+
+                      return SearchWidget(messages[index]);
+
+                    },
+
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
